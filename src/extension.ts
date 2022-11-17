3a6f74d6d4433af9ccd2744995d0f6aa2b489a04
Nestor <elnestinatorHD@gmail.com>
2022-11-17 19:53:36 +0100

    Messing around with custom webviews and styles

--- src/extension.ts
+++ src/extension.ts
@@ -5,24 +5,60 @@ import * as vscode from "vscode";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "macglass" is now active!');
+  if (process.platform === "win32") {
+    vscode.window.showErrorMessage(
+      "This extension is currently not supported on Windows.",
+    );
+  }
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    "macglass.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from macglass!");
+  const jsonStyle = {
+    "customizeUI.stylesheet": {
+      "body.inline-title-bar .monaco-workbench:not(.fullscreen) .sidebar .composite.title":
+        "background: rgba(0, 0, 0, 0) !important;",
+      ".statusbar-item.left.has-background-color.first-visible-item":
+        "background-color: rgba(37, 37, 38, 0) !important;",
+      ".monaco-workbench .part > .title":
+        "background: rgba(0, 0, 0, 0) !important;",
+      "body.inline-title-bar:not(.activity-bar-at-bottom) .monaco-workbench:not(.fullscreen) .activitybar:not(.right) .activity-bar-placeholder":
+        "background: rgba(0, 0, 0, 0) !important;",
+      ".monaco-list .monaco-list-row": "border-radius: 6px !important;",
+      ".part.statusbar": "background-color: rgba(37, 37, 38, 0) !important;",
+      ".monaco-workbench .part.panel":
+        "border-radius: 6px !important; margin-top: 6px !important; height: calc(100% - 6px) !important; width: calc(100% - 6px) !important; box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24) !important;",
+      "#workbench.parts.editor > .content":
+        "border-bottom-left-radius: 6px !important;",
+      ".monaco-workbench .part.panel.bottom .composite.title":
+        "border-top-color: rgba(128, 128, 128, 0) !important;",
+      ".parts.editor > div.content > div.grid-view-container > div > div > div > div.split-view-container > div":
+        "width: calc(100% - 6px) !important;",
+      ".part.editor.has-watermark":
+        "height: calc(100% - 6px) !important; margin-top: 6px !important; border-radius: 6px !important; width: calc(100% - 6px) !important; box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24) !important;",
     },
+  };
+
+  const styleString = Object.entries(jsonStyle)
+    .map(([k, v]) => `${k}:${v}`)
+    .join(";");
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("macglass.start", () => {
+      const style = vscode.window.createWebviewPanel(
+        "style",
+        "Style",
+        vscode.ViewColumn.One,
+        {
+          enableScripts: true,
+        },
+      );
+      style.webview.html = `
+    <html>
+      <head>
+        <style>
+        ${styleString}
+        </style>
+      </head>
+    </html>
+  `;
+    }),
   );
-
-  context.subscriptions.push(disposable);
 }
-
-// This method is called when your extension is deactivated
-// export function deactivate() {}
